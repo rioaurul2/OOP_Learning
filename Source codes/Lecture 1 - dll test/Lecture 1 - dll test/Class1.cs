5b7f0fe6999a8080310a5cc15aba94bd369fbf3c
Marin Pop <pmarin08@yahoo.com>
2023-11-24 20:55:13 +0200

    add lecture 1

---
+++ Source codes/Lecture 1 - dll test/Lecture 1 - dll test/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace Lecture_1___dll_test
+{
+    public class Dll_PrintToScreenRandomNumber
+    {
+        Random myRandomGen = new();
+
+        public virtual void PrintToScreen()
+        {
+            Console.WriteLine(myRandomGen.Next().ToString("N0"));
+            printToScreenPrivate();
+        }
+        internal void PrintToScreenIntternal()
+        {
+            Console.WriteLine("Message from dll Internal Modifier:");
+            printToScreenPrivate();
+        }
+
+        private void printToScreenPrivate()
+        {
+            Console.WriteLine(" dll It's me, Private" + myRandomGen.Next().ToString("N3"));
+        }
+
+        protected void printToScreenProtected()
+        {
+            Console.WriteLine(myRandomGen.Next().ToString("N6"));
+        }
+    }
+}
