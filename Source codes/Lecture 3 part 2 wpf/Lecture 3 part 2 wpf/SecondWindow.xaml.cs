7a7890430362801ba872537002d941a710c866de
Marin Pop <pmarin08@yahoo.com>
2023-11-29 18:57:35 +0200

    save lecture 3 wpf example

--- Source codes/Lecture 3 part 2 wpf/Lecture 3 part 2 wpf/SecondWindow.xaml.cs
+++ Source codes/Lecture 3 part 2 wpf/Lecture 3 part 2 wpf/SecondWindow.xaml.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace Lecture_3_part_2_wpf
 {
@@ -22,6 +18,18 @@ namespace Lecture_3_part_2_wpf
         public SecondWindow()
         {
             InitializeComponent();
+            sortOrder.Items.Add("Orber by");
+            sortOrder.Items.Add("Orber by keys ascending");
+            sortOrder.Items.Add("Orber by keys descending");
+            sortOrder.Items.Add("Orber by values ascending");
+            sortOrder.Items.Add("Orber by values descending");
+            sortOrder.SelectedIndex = 0;
+
+            fileWritingMethod.Items.Add("Orber by writing method");
+            fileWritingMethod.Items.Add("use Write all lines");
+            fileWritingMethod.Items.Add("use streamWriter method");
+            fileWritingMethod.Items.Add("use stringBuilder method");
+            fileWritingMethod.SelectedIndex = 0;
         }
 
         Dictionary<int, int> generatedNumbers = new();
@@ -30,9 +38,11 @@ namespace Lecture_3_part_2_wpf
         {
             Random randomGenerator = new();
 
-            for (int i = 0; i < 100; i++)
+            generatedNumbers.Clear();
+
+            for (int i = 0; i < 10000; i++)
             {
-                int randomNumber = randomGenerator.Next(1, 100);
+                int randomNumber = randomGenerator.Next(1, 10000);
 
                 if (generatedNumbers.ContainsKey(randomNumber)) {
                     generatedNumbers[randomNumber] += 1;
@@ -63,6 +73,136 @@ namespace Lecture_3_part_2_wpf
         {
             MainWindow window = new ();
             window.Show();
+        }
+
+        private void orderBy_Click(object sender, RoutedEventArgs e)
+        {
+            switch (sortOrder.SelectedIndex)
+            {
+                case 0:
+                    MessageBox.Show("No sorting option selected");
+                    break;
+                case 1:
+                    generatedNumbers = generatedNumbers.OrderBy(val => val.Key).ToDictionary(val=> val.Key, val=> val.Value);
+                    break;
+                case 2:
+                    generatedNumbers = generatedNumbers.OrderByDescending(val => val.Key).ToDictionary(val => val.Key, val => val.Value);
+                    break;
+                case 3:
+                    generatedNumbers = generatedNumbers.OrderBy(val => val.Value).ToDictionary(val => val.Key, val => val.Value);
+                    break;
+                case 4:
+                    generatedNumbers = generatedNumbers.OrderByDescending(val => val.Value).ToDictionary(val => val.Key, val => val.Value);
+                    break;
+            }
+        }
+
+        private void writeToFile_Click(object sender, RoutedEventArgs e)
+        {
+            switch (fileWritingMethod.SelectedIndex)
+            {
+                case 0:
+                    MessageBox.Show("No writing method selected");
+                    break;
+                case 1:
+                    WriteFileWithAllLines();
+                    break;
+                case 2:
+                    WriteFileWithStreamWriterNoAutoFlush();
+                    WriteFileWithStreamWriterAutoFlush();
+                    break;
+                case 3:
+                    WriteStringBuilder();
+                    break;
+            }
+
+            var exePath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Process.Start("explorer.exe",exePath);
+            GC.Collect();
+        }
+
+        private void WriteFileWithAllLines()
+        {
+            string fileName = "WriteAllLines.txt";
+            File.Delete(fileName);
+
+            Stopwatch timer = new();
+            timer.Start();
+
+            File.WriteAllLines(fileName, generatedNumbers.Select(pr => pr.Key + "\t" + pr.Value));
+
+            timer.Stop();
+
+            lstBoxStatictics.Items.Insert(0, "WriteFileWithAllLines took :" + timer.ElapsedMilliseconds.ToString("N0") + " ms");
+        }
+
+        private void WriteFileWithStreamWriterNoAutoFlush()
+        {
+            string fileName = "StreamWriterNoAutoFlush.txt";
+            File.Delete(fileName);
+
+            StreamWriter swWrite = new(fileName);
+
+            Stopwatch timer = new();
+            timer.Start();
+
+            foreach(var pair in generatedNumbers)
+            {
+                swWrite.WriteLine($"{pair.Key}\t{pair.Value}");
+            }
+
+            swWrite.Flush();
+            swWrite.Close();
+
+            timer.Stop();
+
+            lstBoxStatictics.Items.Insert(0, "WriteFileWithStreamWriterNoAutoFlush took :" + timer.ElapsedMilliseconds.ToString("N0") + " ms");
+        }
+
+        private void WriteFileWithStreamWriterAutoFlush()
+        {
+            string fileName = "StreamWriterAutoFlush.txt";
+            File.Delete(fileName);
+
+            StreamWriter swWrite = new(fileName);
+            swWrite.AutoFlush = true;
+
+            Stopwatch timer = new();
+            timer.Start();
+
+            foreach (var pair in generatedNumbers)
+            {
+                swWrite.WriteLine($"{pair.Key}\t{pair.Value}");
+            }
+
+            swWrite.Flush();
+            swWrite.Close();
+
+            timer.Stop();
+
+            lstBoxStatictics.Items.Insert(0, "WriteFileWithStreamWriterAutoFlush took :" + timer.ElapsedMilliseconds.ToString("N0") + " ms");
+        }
+
+        private void WriteStringBuilder()
+        {
+            string fileName = "StringBuilder.txt";
+            File.Delete(fileName);
+
+            StringBuilder strBuilder = new();
+
+            Stopwatch timer = new();
+            timer.Start();
+
+            foreach (var pair in generatedNumbers)
+            {
+                strBuilder.AppendLine($"{pair.Key}\t{pair.Value}");
+            }
+
+            File.WriteAllText(fileName, strBuilder.ToString());
+
+            timer.Stop();
+
+            lstBoxStatictics.Items.Insert(0, "WriteStringBuilder took :" + timer.ElapsedMilliseconds.ToString("N0") + " ms");
         }
     }
 }
--- Source codes/Lecture 3 part 2 wpf/Lecture 3 part 2 wpf/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Source codes/Lecture 3 part 2 wpf/Lecture 3 part 2 wpf/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -149,7 +149,7 @@ namespace Lecture_3_part_2_wpf {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Lecture 3 part 2 wpf;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Lecture 3 part 2 wpf;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
