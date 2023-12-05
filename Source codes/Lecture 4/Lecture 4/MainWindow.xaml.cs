2b00ec9a0886be7871a101d4e0f630e453a88a6e
Marin Pop <pmarin08@yahoo.com>
2023-12-06 01:39:41 +0200

    save changes(Stack/Heap/GC example)

---
+++ Source codes/Lecture 4/Lecture 4/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime;
+using System.Windows;
+
+namespace Lecture_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Random is a class thath means that is a reference type
+        static Random randomNumber = new ();
+
+        //It's a method that will be executed in the stack
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 10000000; i++)
+            {
+                //double is a struct that means that is a value type
+                double val = randomNumber.NextDouble();
+            }
+        }
+
+        public class ReferenceDoubleClassTest
+        {
+            public double val;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (GC.TryStartNoGCRegion(9999999, true))
+            {
+                try
+                {
+                    for (int i = 0; i < 10000000; i++)
+                    {
+                        TestReferenceTypeCall();
+                    }
+                }
+                finally
+                {
+
+                }
+            }
+        }
+
+        private void TestReferenceTypeCall()
+        {
+            ReferenceDoubleClassTest testVal = new() { val = randomNumber.NextDouble() };
+        }
+    }
+}
---
+++ Source codes/Lecture 4/Lecture 4/ValueTypes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_4
+{
+    class ValueTypes
+    {
+        bool boolValueType = false;//by default
+        byte byteValueType = 0;//by default
+        char charValueType = '\0';//by default
+        decimal decimalValueType = 0;//by default
+        double doubleValueType = 0;//by default
+        enum enumValueType
+        {
+            enumValueType0 = 0,
+            enumValueType1 = 1,
+        }
+        float floatValueType = 0;//by default
+        int intValueType = 0;
+        long longValueType = 0;
+        sbyte sbyteValueType = 0;
+        short shortValueType = 0;
+        struct structValueType
+        {
+            string name;
+            int age;
+        }
+
+        uint uintValueType = 0;
+        ulong ulongValueType = 0;
+        ushort ushortValueType = 0;
+    }
+
+    class referenceTypes
+    {
+        class classReferenceType
+        {
+
+        }//empty
+
+        interface IInterfaceReferenceType {
+            void ReferenceMethodExample();
+        }
+
+        public delegate void delegateReferenceType(string msg);
+
+        object objectReferenceType;
+        string stringReferenceType;
+    }
+}
