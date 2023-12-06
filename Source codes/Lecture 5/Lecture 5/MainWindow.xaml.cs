1e57360031e1c755f25623a99614f1f14e5e863e
Marin Pop <pmarin08@yahoo.com>
2023-12-06 23:38:30 +0200

    Save changes

---
+++ Source codes/Lecture 5/Lecture 5/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<int> mainList = new(); 
+        Random randomNumbers = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitList();
+        }
+
+        private void shallowClonning_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> secondList = mainList;
+
+            secondList[2] = 2000;
+
+            //Error due shallow clonning (The lists have the same reference so when one is changed bhoth are changed)
+            //foreach (int number in secondList) {
+
+            //    mainList.Add(number);
+            //}
+
+            RefreshListBox();
+        }
+
+        private void InitList()
+        {
+            for(int i = 1; i < 10; i++) 
+            {
+                mainList.Add(i*10);
+            }
+        }
+
+        private void RefreshListBox()
+        {
+            listResults.Items.Clear();
+            foreach (int number in mainList) {
+
+                listResults.Items.Add(number);
+            }
+
+        }
+
+        private void refreshListBox_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshListBox();
+        }
+
+        private void clearBoxList_Click(object sender, RoutedEventArgs e)
+        {
+            listResults.Items.Clear();
+        }
+    }
+}
